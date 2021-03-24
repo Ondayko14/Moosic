ae9ad63fcc742a44734766b48e76b350a623d956
Andrew Ahler <andrewahler@gmail.com>
2021-03-24 15:38:05 -0500

    added to typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    posts: [Posts]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addPosts(posts: [ID]!): Posts
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updatePosts(_id: ID!, quantity: Int!): Posts
+    login(email: String!, password: String!): Auth
+  }
+
+
+
+
+`;
+
+module.exports = typeDefs;
