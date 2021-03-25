c2c758313c9420c0f65b343a3ce6bbf5d927277d
jelly octane <jamie.de.lange@mac.com>
2021-03-25 17:38:06 -0500

    create client directory, install dependencies, begin webpack implementation

---
+++ client/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: './src/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static"
+        })
+    ],
+    mode: 'development'
+};
