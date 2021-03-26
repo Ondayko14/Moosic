20ce8285eccf73024d0f5ccbe048fb52ff4440d9
jelly octane <jamie.de.lange@mac.com>
2021-03-25 19:20:48 -0500

    some tweaks

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,15 @@ const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './src/script.js',
+    entry: {
+        app: './src/script.js',
+        home: './src/pages/Home.js',
+        post: './src/pages/Post.js',
+        profile: './src/pages/Profile.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
     },
     plugins: [
         new webpack.ProvidePlugin({
