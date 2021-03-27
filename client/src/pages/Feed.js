9337bd03bc392880f21be296ccaae85ddb4157af
jelly octane <jamie.de.lange@mac.com>
2021-03-27 11:39:29 -0400

    preliminary pages/components setup begun

---
+++ client/src/pages/Feed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import Auth from '../utils/auth';
+import Header from '../components/Header';
+
+const Feed = () => {
+
+    return (
+        <main>
+
+        </main>
+    )
+}
+
+export default Feed;
