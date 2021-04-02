2b1424ad13d8c0c9e705b890d2fe85433ef850fa
jelly octane <jamie.de.lange@mac.com>
2021-04-02 12:08:18 -0500

    fixed login

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,24 @@
 const { Schema, model, Types } = require("mongoose");
+const bcrypt = require('bcrypt');
 
-const UserSchema = new Schema(
+const userSchema = new Schema(
   {
     key: {
         type: Schema.Types.ObjectId,
         default: () => new Types.ObjectId()
     },
-    userName: {
-      type: String,
-    },
-    password: {
+    username: {
       type: String,
     },
     email: {
       type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
     },
     },
   
@@ -26,5 +31,18 @@ const UserSchema = new Schema(
   }
 );
 
-const User = model("User", UserSchema);
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
 module.exports = User;
